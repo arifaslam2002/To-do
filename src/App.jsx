8f0bd1e31ad78bfd492a905620d6a67cf28d5eb6
Arif A <aarifaslam2002@gmail.com>
2025-02-06 12:47:47 +0530

    Upadate Component folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import Nav from './Component/Navbar/Nav'
-import Footer from './Component/Footer/Footer'
-import Todolist from './Pages/lists/lists'
+import Nav from './Component/Navbar/Nav';
+import Footer from './Component/Footer/Footer';
+import Todolist from './Pages/lists/lists';
 import './App.css'
 const App = () => {
   return (
